70d643b7a40be48490314187c7efb731fc54292e
Arju Srivastava <krishnabhakt677@gmail.com>
2026-04-23 15:11:08 +0530

    Initial broken CI setup

---
+++ test_app.py
@@ -0,0 +1,14 @@
+from app import add, divide
+
+def test_add():
+    assert add(2,3) == 6
+
+def test_divide():
+    assert divide(10,2) == 5
+
+def test_divide_by_zero():
+    divide(5,0)
+
+def test_add_string():
+    assert add("a ", "b") == "ab"
+
