43e33e7e3d4997a52be103025e57723b9be87478
Arju Srivastava <krishnabhakt677@gmail.com>
2026-05-02 01:40:26 +0530

    [WIP] Automated fix attempt by healpipe for job 3db2db55272f4bbc828b96418427c6da

--- app.py
+++ app.py
@@ -6,12 +6,8 @@ def add(a, b):
 def divide(a, b):
     return np.divide(a, b)
 
-def sum(a,b){
-    return a+b;
-}
+def sum(a, b):
+    return a + b
 
-for (int i=0; i<10; i++){
-    print(i);
-}
-
-lolu
+for i in range(10):
+    print(i)
